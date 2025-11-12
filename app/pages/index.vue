de993892f3a2f2de1f98f127663804be70cc0e40
Brice <lebowvsky@gmail.com>
2025-11-13 00:11:18 +0100

    feat: :sparkles: composables first

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,13 @@
 <template>
   <h1>Home</h1>
   <hr />
+  <ul v-if="!users.loader.value">
+    <li v-for="user in users.names.value" :key="user">{{ user }}</li>
+  </ul>
+  <p v-else>Loading...</p>
+
+  <button @click="users.addUser">Ajouter</button>
 </template>
+<script setup>
+const users = getUsers("Miles");
+</script>
